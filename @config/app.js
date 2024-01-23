fea618ae38c3e32e74c5a3f40233ab787e0c6020
JakedUp <JakeSiemer@Gmail.com>
2024-01-23 15:15:55 -0500

    Adding provider blacklist to configurator

--- @config/app.js
+++ @config/app.js
@@ -1,12 +1,47 @@
 const App = Vue.createApp({
   data() {
     return {
+      url: {
+        server: 'https://syncler-providers.herokuapp.com/syncler'
+      },
+      express: {
+        packages: {
+          'OpenScrapers': 'https://raw.githubusercontent.com/SynclerScrapers/OpenScrapers/main/express/openscraper.json',
+          'Orion - Wako': 'https://wako.orionoid.com/ABCDEFGHIJKLMNOPQRSTUVWXYZ123456',
+          'Squizzle - Fast': 'https://raw.githubusercontent.com/providers4syncler/providers/main/fast',
+          'Squizzle - Ultimate': 'https://raw.githubusercontent.com/providers4syncler/providers/main/squizzle',
+          'Aki07 - Unified': 'https://raw.githubusercontent.com/itzAki07/scraper/main/scraper.json',
+        },
+        providers: {},
+        blacklist: []
+      },
       orion: {
         apiKey: '',
         commonProviders: 1
       },
+      kosmos: {
+        packages: {
+          /*
+          'WeeklyProblem - Bouncy': 'https://raw.githubusercontent.com/WeeklyProblem/Bouncy/main/bouncy.min.js'
+          */
+        }
+      },
       justwatch: {
         mode: 'default',
+        providers: {
+          'netflix': 'Netflix',
+          'hulu': 'Hulu',
+          'amazon-prime-video': 'Amazon Prime Video',
+          'disney': 'Disney',
+          'hbo-max': 'HBO Max',
+          'showtime-anytime': 'Showtime Anytime',
+          'fxnow': 'FX Now',
+          'crackle': 'Crackle',
+          'tubi': 'Tubi',
+          'vudu': 'Vudu',
+          'google-tv': 'Google TV',
+          'youtube': 'YouTube',
+        },
         blacklist: [],
         whitelist: []
       }
@@ -21,14 +56,17 @@ const App = Vue.createApp({
       if (!this.orion.commonProviders) {
         params['exclude-orion'] = true;
       }
+      if (this.express.blacklist.length) {
+        params.blacklist = this.express.blacklist.join(',');
+      }
       const query = new URLSearchParams(params).toString().replace(/%2C/g, ',');
       return query ? `?${query}` : '';
     },
     vendorUrl() {
-      return `https://syncler-providers.herokuapp.com/syncler/vendor-hybrid.json${this.expressQuery}`
+      return `${this.url.server}/vendor-hybrid.json${this.expressQuery}`
     },
     expressUrl() {
-      return `https://syncler-providers.herokuapp.com/syncler/express-hybrid.json${this.expressQuery}`
+      return `${this.url.server}/express-hybrid.json${this.expressQuery}`
     },
     justwatchQuery() {
       const params = {};
@@ -42,7 +80,7 @@ const App = Vue.createApp({
       return query ? `?${query}` : '';
     },
     justwatchUrl() {
-      return `https://syncler-providers.herokuapp.com/syncler/kosmos-justwatch.js${this.justwatchQuery}`
+      return `${this.url.server}/kosmos-justwatch.js${this.justwatchQuery}`
     }
   },
   watch: {
@@ -52,6 +90,26 @@ const App = Vue.createApp({
         this.orion.commonProviders = 1;
       }
     }
+  },
+  methods: {
+    getProviders: function () {
+      return fetch(this.expressUrl)
+        .then(response => response.json())
+        /*
+        .then(json => Object.keys(json).filter(key => !key.includes('_manifest')).reduce((result, key) => ({...result, [key]: json[key].name}), {}));
+        */
+        .then(json => Object.fromEntries(Object.entries(json).filter(([key, value]) => key !== '_manifest')));
+    }
+  },
+  created() {
+    this.getProviders().then((providers) => {
+      /*
+      providers.orion = {
+        name: 'Orion'
+      };
+      */
+      this.express.providers = providers;
+    });
   },
   mounted() {
     let $navTabs = jQuery(`.nav-tabs .nav-link`);
